a998020b40ea389649860805acc55790cd7ef154
Tunghsu <tunghsu@outlook.com>
2013-11-23 19:42:28 +0800

    Initilized Component

--- GOS/settings.py
+++ GOS/settings.py
@@ -109,6 +109,7 @@ ROOT_URLCONF = 'GOS.urls'
 WSGI_APPLICATION = 'GOS.wsgi.application'
 
 TEMPLATE_DIRS = (
+                 '/home/tunghsu/workspace/GOS/templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
